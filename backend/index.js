c097b6aba9b552b8a5da909912a33685d0e33b29
alwaysinmy-rhythm <rdhmpanchal@gmail.com>
2024-03-27 23:36:49 +0530

    -

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin:'https://irctc-three.vercel.app', 
-    credentials:true,           
+    credentials:false,           
     optionSuccessStatus:200
   }));
 
