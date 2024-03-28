4c40b50a0b2ef730c36bdc3a81d075bc5c375c6c
alwaysinmy-rhythm <rdhmpanchal@gmail.com>
2024-03-28 13:11:21 +0530

    -

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,10 @@ const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
-  });
+  })
 // const corsOptions ={
 //     //thanks to claude ai which helped me in this. 
 //     origin: 'https://irctc-three.vercel.app/', // or a list of allowed origins
