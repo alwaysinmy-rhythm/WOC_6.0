7800aab2e352ae79c4b058c77e918d9b7d0ddd67
alwaysinmy-rhythm <rdhmpanchal@gmail.com>
2024-03-28 13:06:21 +0530

    -

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,14 @@ const PORT = 3001;
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 // const corsOptions ={
 //     //thanks to claude ai which helped me in this. 
 //     origin: 'https://irctc-three.vercel.app/', // or a list of allowed origins
